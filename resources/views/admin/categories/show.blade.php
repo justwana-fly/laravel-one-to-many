0fcd1d9bdf4fb7cd03bec64844f4e17d5c3cb9d6
Nicolae <nicolaibarbarasa@gmail.com>
2024-06-05 16:46:11 +0200

    git add resources/views/admin/categories/show.blade.php git commit -m "Display associated type in category detail view"

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -6,60 +6,56 @@
     <div class="d-flex justify-content-between align-items-center py-4">
         <h1>{{$category->name}}</h1>
         <div>
-            <a href="{{route('admin.categories.edit',$category->slug)}}" class="btn btn-secondary">Edit</a>
+            <a href="{{route('admin.categories.edit', $category->slug)}}" class="btn btn-secondary">Edit</a>
             <form action="{{route('admin.categories.destroy', $category->slug)}}" method="POST" class="d-inline-block">
                 @csrf
                 @method('DELETE')
-                <button type="submit" class="delete-button btn btn-danger"  data-item-title="{{ $category->name }}">
-                 Delete Category</i>
+                <button type="submit" class="delete-button btn btn-danger" data-item-title="{{ $category->name }}">
+                    Delete Category
                 </button>
-
-              </form>
+            </form>
         </div>
-
     </div>
 
-
+    <!-- Visualizzare la tipologia associata se presente -->
+    @if($category->type)
+        <p>Type: {{ $category->type->name }}</p>
+    @endif
 
     <table class="table table-striped">
         <thead>
             <tr>
-              <th scope="col">Id</th>
-              <th scope="col">Title</th>
-              <th scope="col">Slug</th>
-              <th scope="col">Created At</th>
-              <th scope="col">Update At</th>
-              <th scope="col">Action</th>
+                <th scope="col">Id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Created At</th>
+                <th scope="col">Update At</th>
+                <th scope="col">Action</th>
             </tr>
-          </thead>
-          <tbody>
+        </thead>
+        <tbody>
             @foreach ($category->posts as $post)
-            <tr>
-                <td>{{$post->id}}</td>
-                <td>{{$post->title}}</td>
-                <td>{{$post->slug}}</td>
-                <td>{{$post->created_at}}</td>
-                <td>{{$post->updated_at}}</td>
-                <td>
-                    <a href="{{route('admin.posts.show', $post->slug)}}" title="Show" class="text-black px-2"><i class="fa-solid fa-eye"></i></a>
-                    <a href="{{route('admin.posts.edit', $post->slug)}}" title="Edit" class="text-black px-2"><i class="fa-solid fa-pen"></i></a>
-                    <form action="{{route('admin.posts.destroy', $post->slug)}}" method="POST" class="d-inline-block">
-                      @csrf
-                      @method('DELETE')
-                      <button type="submit" class="delete-button border-0 bg-transparent"  data-item-title="{{ $post->title }}" data-item-id = "{{ $post->id }}">
-                        <i class="fa-solid fa-trash"></i>
-                      </button>
-
-                    </form>
-
-
-                </td>
-              </tr>
+                <tr>
+                    <td>{{$post->id}}</td>
+                    <td>{{$post->title}}</td>
+                    <td>{{$post->slug}}</td>
+                    <td>{{$post->created_at}}</td>
+                    <td>{{$post->updated_at}}</td>
+                    <td>
+                        <a href="{{route('admin.posts.show', $post->slug)}}" title="Show" class="text-black px-2"><i class="fa-solid fa-eye"></i></a>
+                        <a href="{{route('admin.posts.edit', $post->slug)}}" title="Edit" class="text-black px-2"><i class="fa-solid fa-pen"></i></a>
+                        <form action="{{route('admin.posts.destroy', $post->slug)}}" method="POST" class="d-inline-block">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="delete-button border-0 bg-transparent" data-item-title="{{ $post->title }}" data-item-id="{{ $post->id }}">
+                                <i class="fa-solid fa-trash"></i>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
             @endforeach
-
-
-          </tbody>
-      </table>
+        </tbody>
+    </table>
 </section>
 @include('partials.modal-delete')
 @endsection
