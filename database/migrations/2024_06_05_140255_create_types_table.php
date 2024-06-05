55f6ce250b1d7b5b2d4e7e8e580ccca3fcc4ddcc
Nicolae <nicolaibarbarasa@gmail.com>
2024-06-05 16:11:05 +0200

    git add database/migrations/xxxx_xx_xx_xxxxxx_create_types_table.php git commit -m "Create migration for types table"

--- database/migrations/2024_06_05_140255_create_types_table.php
+++ database/migrations/2024_06_05_140255_create_types_table.php
@@ -4,15 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+        // Creare la tabella 'types' con colonne 'id' e 'name'
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
             $table->timestamps();
         });
     }
@@ -22,6 +23,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        // Eliminare la tabella 'types' in caso di rollback
         Schema::dropIfExists('types');
     }
 };
